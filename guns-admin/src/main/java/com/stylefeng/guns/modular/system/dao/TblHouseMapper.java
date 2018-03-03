b3992eddc8e77b642fd21d558fb72262be10c653
wuqingvika <526288022@qq.com>
2018-03-03 13:26:32 +0800

    guns房屋管理功能查询

---
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/system/dao/TblHouseMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.modular.system.dao;
+
+import com.stylefeng.guns.modular.system.model.TblHouse;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Wuqingvika123
+ * @since 2018-03-03
+ */
+public interface TblHouseMapper extends BaseMapper<TblHouse> {
+
+}
